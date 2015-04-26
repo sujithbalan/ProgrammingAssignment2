44bc5dbc6febbf6ad0b6134c5d1cc7fabe1b13a7
Sujith Balan <sujith.balan@gmail.com>
2015-04-25 21:17:14 -0700

    Initial Change
    Place holder comments and functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R functions to cache the inverse of the matrix, this would avoid performing the
+## costly computation repeatedly, espectially when the contents of the matrix have not changed
+## the cache can be looked up instead.
 
-## Write a short comment describing this function
-
+## Function to create a special matrix that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of the special matrix returned by makeCacheMatrix, if the inverse is alreade
+##calculated then inverse matrix the retreived from cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
